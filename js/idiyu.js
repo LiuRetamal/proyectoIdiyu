18bf8f0286b78b87bf54960f734d5437ce6e90c7
Liu <retamaliu@gmail.com>
2022-10-25 13:49:19 +0200

    Actualizacion paises

---
+++ js/idiyu.js
@@ -0,0 +1,30 @@
+//Menu
+function openNav() {
+document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+// Boton
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    } else {
+    mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
